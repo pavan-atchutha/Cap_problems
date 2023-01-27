f5e664d8de2cfb6b7ab24bece360517c0317a690
PAVAN KUMAR ATCHUTHA <85385993+pavan-atchutha@users.noreply.github.com>
2023-01-27 12:58:37 +0530

    Add files via upload

---
+++ 27-01-2023/binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public HashMap<Integer,List<Integer>> tl(TreeNode root,int s,HashMap<Integer,List<Integer>> h){
+        List<Integer> u =new ArrayList<>();
+        
+        if(root!=null){
+            u.add(root.val);
+            if(h.containsKey(s)){
+                (h.get(s)).addAll(u);}
+            else{
+                h.put(s,u);
+            }
+           h= tl(root.left,s+1,h);
+           h=tl(root.right,s+1,h);
+        }
+        return h;
+
+    }
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        HashMap<Integer,List<Integer>> l= new HashMap<>();
+        List<List<Integer>> j=new ArrayList<>();
+        int s=0;
+        if(root==null){
+            return j;
+        }
+        l=tl(root,s,l);
+        for ( List<Integer> u: l.values()) 
+            j.add(u);
+        Collections.reverse(j);
+        return j;
+    }
+        
+}
